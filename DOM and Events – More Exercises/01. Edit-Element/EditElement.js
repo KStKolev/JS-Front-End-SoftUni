8f9be42fe2f18defa5b3eba9a3d4015262a53da0
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2023-11-07 14:15:50 +0200

    Delete DOM and Events – More Exercises directory

--- DOM and Events – More Exercises/01. Edit-Element/EditElement.js
+++
@@ -1,5 +0,0 @@
-function editElement(element, originalText, replacedText) {
-    let elementText = element.textContent;
-    elementText = elementText.split(originalText).join(replacedText);
-    element.textContent = elementText;
-}
--- DOM and Events – More Exercises/02. Extract-Parenthesis/ExtractParenthesis.js
+++
@@ -1,6 +0,0 @@
-function extract(content) {
-    const elementText = document.querySelector(`#${content}`);
-    const matchWords = /\([a-zA-z ]+\)/gm;
-    let matchedWords = elementText.textContent.match(matchWords);
-    return matchedWords.join("; ");
-}
